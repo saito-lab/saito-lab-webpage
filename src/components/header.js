a5a2b95ece8827943d92a77a2cdd405279859d2d
MiuKanematsu <miu.kanema@gmail.com>
2025-02-06 23:13:15 +0900

    fix logo path

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,15 @@
 import * as React from "react"
 import * as styles from "./header.module.scss"
 import { Link } from "gatsby"
+// import { GatsbyImage} from "gatsby-plugin-image"
+import { StaticImage} from "gatsby-plugin-image"
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.logoContainer}>
                 <Link to="/">
-                    <img src="/logo-large.jpg" className={styles.logo} />
+                    <StaticImage src="../images/logo-large.jpg" alt="logo" className={styles.logo} height={70}/>
                 </Link>
             </div>
             <div className={styles.linkContainer}>
